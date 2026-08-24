6301940f62c1532722a40bd4c7ce7a546367588b
KyawZay1979 <kyawzay1979@gmail.com>
2026-08-24 21:43:57 +0630

    fix syntax error in AppServiceProvider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -22,5 +22,6 @@ class AppServiceProvider extends ServiceProvider
         // Production Environment (Render) ပေါ်ရောက်ပါက HTTPS သို့ Force ပြောင်းခိုင်းခြင်း
         if (config('app.env') === 'production' || $this->app->environment('production')) {
             URL::forceScheme('https');
+        }
     }
 }
